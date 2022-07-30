16c34f6a90502f5b40b9dc5667504d063c93356f
manicsatanic <manicsatanic@example.com>
2022-07-30 10:33:13 -0700

    goblin-roguelike initial commit

---
+++ map.js
@@ -0,0 +1,13 @@
+function generateLevel() {
+    tiles = [];
+    for (let i = 0; i < numTiles; i++) {
+        tiles[i] = [];
+        for (let j = 0; j < numTiles; j++) {
+            if (Math.random() < 0.3) {
+                tiles[i][j] = new Wall(i, j);
+            } else {
+                tiles[i][j] = new Floor(i, j);
+            }
+        }
+    }
+}
---
+++ tile.js
@@ -0,0 +1,25 @@
+class Tile {
+    constructer(x, y, sprite, passable) {
+        this.x = x;
+        this.y = y;
+        this.sprite = sprite;
+        this.passable = passable;
+
+    }
+
+    draw() {
+        drawSprite(this.sprite, this.x, this.y);
+    }
+}
+
+class Floor extends Tile {
+    constructor(x, y) {
+        super(x, y, 2, true);
+    };
+}
+
+class Wall extends Tile {
+    constructor(x, y) {
+        super(x, y, 3, false);
+    }
+}
